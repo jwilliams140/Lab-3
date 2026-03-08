383a6eb596170e59c51d2617b763f23d49d78d91
jwilliams140 <jwilliams140@csub.edu>
2026-03-08 13:34:34 -0700

    Fixed error when starting expo go app

--- PokemonBuilder.ts
+++ PokemonBuilder.ts
@@ -2,35 +2,40 @@ import { Pokemon } from "./Pokemon";
 
 export class PokemonBuilder {
 
-    private pokemon: Pokemon = {
-        name: "",
-        image: "",
-        types: [],
-        abilities: [],
-        moves: []
-    };
+  private pokemon: Pokemon = {
+    name: "",
+    image: "",
+    types: [],
+    abilities: [],
+    moves: []
+  };
 
-    setName(name: string) {
-        this.pokemon.name = name;
-        return this;
-    }
+  setName(name: string) {
+    this.pokemon.name = name;
+    return this;
+  }
 
-    setImage(image: string) {
-        this.pokemon.image = image;
-        return this;
-    }
+  setImage(image: string) {
+    this.pokemon.image = image;
+    return this;
+  }
 
-    setAbilities(abilities: string[]) {
-        this.pokemon.abilities = abilities;
-        return this;
-    }
+  setTypes(types: string[]) {
+    this.pokemon.types = types;
+    return this;
+  }
 
-    setMoves(moves: string[]) {
-        this.pokemon.moves = moves;
-        return this.pokemon;
-    }
+  setAbilities(abilities: string[]) {
+    this.pokemon.abilities = abilities;
+    return this;
+  }
 
-    build(): Pokemon {
-        return this.pokemon;
-    }
+  setMoves(moves: string[]) {
+    this.pokemon.moves = moves;
+    return this;
+  }
+
+  build(): Pokemon {
+    return this.pokemon;
+  }
 }

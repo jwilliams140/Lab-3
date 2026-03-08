eaadda082a08f8168c6356671b64f163cb5eb6af
jwilliams140 <jwilliams140@csub.edu>
2026-03-08 13:33:42 -0700

    Fixed error when starting expo go app

--- pokemonApi.ts
+++ pokemonApi.ts
@@ -1,19 +1,31 @@
+import { PokemonBuilder } from "../models/PokemonBuilder";
+
 export async function fetchPokemon(name: string) {
-    const q = name.trim().toLowerCase();
 
-    if (!q) {
-        throw new Error("Please enter a Pokémon name");
-    }
+  const q = name.trim().toLowerCase();
 
-    console.log("Fetching Pokémon:", q);
+  if (!q) {
+    throw new Error("Please enter a Pokémon name");
+  }
 
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${q}`);
+  console.log("Fetching pokemon:", q);
 
-    if (!response.ok) {
-        throw new Error("Pokémon not found");
-    }
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${q}`);
 
-    const data = await response.json();
-    console.log("Pokémon data", data);
-    return data;
+  if (!response.ok) {
+    throw new Error("Pokemon not found");
+  }
+
+  const data: any = await response.json();
+
+  const pokemon = new PokemonBuilder()
+    .setName(data.name)
+    .setImage(data.sprites.front_default)
+    .setTypes(data.types.map((t: any) => t.type.name))
+    .setAbilities(data.abilities.map((a: any) => a.ability.name))
+    .setMoves(data.moves.slice(0, 5).map((m: any) => m.move.name))
+    .build();
+
+  return pokemon;
 }
+
